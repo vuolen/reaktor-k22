99c19e97197a4bf9cee9ceb894c385d6a2b8c658
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2021-12-01 14:36:55 +0200

    sync

--- src/Api.purs
+++ src/Api.purs
@@ -1,15 +1,18 @@
 module API where
 
-import Prelude ((<>))
+import Prelude ((<>), bind, ($))
 import Effect.Aff (Aff)
 import Data.DateTime (DateTime)
+import Data.Either (Either(..))
 import Affjax as AX
 import Affjax.ResponseFormat (json)
+import Data.Argonaut (stringify)
 import Effect.Class.Console (log)
 
-_API_URL = "https://bad-api-assignment.reaktor.com"
+_API_URL = "https://bad-api-assignment.reaktor.com" :: String
 
 data RPS = Rock | Paper | Scissors
+
 type GameResult = {
     gameId :: String,
     time :: DateTime,
@@ -23,10 +26,10 @@ type GameResult = {
     }
 }
 
-getFullHistory :: Aff (Array GameResult) 
+getFullHistory :: Either Error (Aff (Array GameResult))
 getFullHistory = getFullHistory_ "/rps/history" []
     where
+        getFullHistory_ :: String -> Array GameResult -> Aff (Array GameResult)
         getFullHistory_ path results = do
             page <- AX.get json (_API_URL <> path)
-            case page of
-                Left err -> log $ "GET " <> path <> " failed: " <> AX.printError err 
+            
