00afb9a1ad1161a23780bcb41fb75c412d921b4a
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-14 18:06:13 +0200

    updated the look of live games

--- src/Rps/UI/LiveGame.purs
+++ src/Rps/UI/LiveGame.purs
@@ -5,7 +5,7 @@ import React
 import Data.Functor ((<$>))
 import Data.Maybe (Maybe(..), fromMaybe)
 import Prelude (map, show, ($), (<>))
-import React.DOM (div, div', h2', text)
+import React.DOM (b', br', div, div', h2', text)
 import React.DOM.Props (className)
 import Rps.Types (LiveGame(..), PlayedGame, RPS(..))
 
@@ -13,14 +13,17 @@ type LiveGameProps = {game :: LiveGame}
 
 liveGame :: ReactClass LiveGameProps
 liveGame = statelessComponent \{game} -> liveGame' game
-    where 
-        liveGame' (InProgress game) = div' [
-            h2' [text $ game.playerA.name <> " vs. " <> game.playerB.name],
+    where
+        liveGame' (InProgress game) = div [className "liveGame"] [
+            b' [text $ game.playerA.name <> " vs. " <> game.playerB.name],
+            br',
             text $ "In progress..."
         ]
         liveGame' (Finished game) = div [className "liveGame"] [
-            h2' [text $ game.playerA.name <> " vs. " <> game.playerB.name],
+            b' [text $ game.playerA.name <> " vs. " <> game.playerB.name],
+            br',
             text $ show game.playerA.played <> " vs. " <> show game.playerB.played,
+            br',
             text $ fromMaybe "Its a tie!" $ (_ <> " wins!") <$> (_.name) <$> (winner game)
         ]
         winner :: PlayedGame -> Maybe {name :: String, played :: RPS}
