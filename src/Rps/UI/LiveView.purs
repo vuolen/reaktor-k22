d51ca37d7d164a36c7d4ce45fe298bf1a9c32ec1
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-01-15 00:33:32 +0200

    formatting and removing unused imports

--- src/Rps/UI/LiveView.purs
+++ src/Rps/UI/LiveView.purs
@@ -2,8 +2,8 @@ module Rps.UI.LiveView where
 
 import React
 
-import Prelude (map, ($), (<>))
-import React.DOM (div, h1', hr', text)
+import Prelude (map, ($))
+import React.DOM (div)
 import React.DOM.Props (className)
 import Rps.Types (LiveGame)
 import Rps.UI.LiveGame (liveGame)
@@ -12,6 +12,6 @@ type LiveViewProps = { liveGames :: Array LiveGame }
 
 liveView :: ReactClass LiveViewProps
 liveView = statelessComponent \{ liveGames } ->
-  div [className "live"] $ map createLiveGame liveGames
+  div [ className "live" ] $ map createLiveGame liveGames
   where
   createLiveGame game = createLeafElement liveGame { game }
